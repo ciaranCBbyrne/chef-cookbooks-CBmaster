2227a772b686496d38ce630478e09a4080bd0039
ciaran <flipdrop@hotmail.com>
2015-05-04 17:57:24 +0100

    Fix bugs

--- files/default/check_slave_capacity.rb
+++ files/default/check_slave_capacity.rb
@@ -29,50 +29,45 @@ class CheckSlave
 				ssh_address =  node_info.ipaddress
 			end
 		end
-		return ssh_address
+		return server_node_id, ssh_address
 	end
 
 	def get_capacity(slave_address)
 
 		output = Hash.new
 		begin
-			Timeout::timeout(10) do
-				begin
-					#open connection through ssh
-					Net::SSH.start("#{slave_address.to_s}", 'ec2-user', :keys => 'MYSSHKEYS') do |ssh|
-						# get system memory info from node in json format
-						taken =  ssh.exec!("sudo ohai filesystem") # see chef-cookbooks-CBsql/recipes/update_sudo_tty.rb or this won't be allowed
-						# convert json to hash
-						output = JSON.parse(taken)
+			#open connection through ssh
+			Net::SSH.start("#{slave_address.to_s}", 'ec2-user', :keys => 'MYSSHKEYS') do |ssh|
+				# get system memory info from node in json format
+				taken =  ssh.exec!("sudo ohai filesystem") # see chef-cookbooks-CBsql/recipes/update_sudo_tty.rb or this won't be allowed
+				# convert json to hash
+				output = JSON.parse(taken)
 
-						# get space details from hash
-						kb_size = output['/dev/xvda1']['kb_size'].to_i
-						kb_used = output['/dev/xvda1']['kb_used'].to_i
-						kb_available = output['/dev/xvda1']['kb_available'].to_i
+				# get space details from hash
+				kb_size = output['/dev/xvda1']['kb_size'].to_i
+				kb_used = output['/dev/xvda1']['kb_used'].to_i
+				kb_available = output['/dev/xvda1']['kb_available'].to_i
 
-						# print free memory of node
-						puts slave_address
-						puts "size: #{kb_size} kb"
-						puts "used: #{kb_used} kb"
-						puts "avail: #{kb_available} kb"
+				# print free memory of node
+				puts slave_address
+				puts
+				puts "size: #{kb_size} kb"
+				puts "used: #{kb_used} kb"
+				puts "avail: #{kb_available} kb"
 
-						# if node is 90% launch new slave
-						$capacity_reached = (kb_used.to_i * 100 ) / kb_size.to_i
-
-						return $capacity_reached
-					end
-				rescue Net::SSH::HostKeyMismatch => e
-					puts "Problem connecting to slave."
-					puts "Please wait and try again"
-					retry
-				rescue StandardError => e
-					puts "Something happened!!!"
-					return 0
-				end
+				# if node is 90% new slave will be launched
+				$capacity_reached = (kb_used.to_i * 100 ) / kb_size.to_i
+				puts
+				puts "#{$capacity_reached}% used"
+				return $capacity_reached
 			end
-		rescue Timeout::Error
-			puts "Timed out trying to connect to slave."
+		rescue Net::SSH::HostKeyMismatch => e
+			puts "Problem connecting to slave."
 			puts "Please wait and try again"
+			retry
+		rescue StandardError => e
+			puts "Something happened!!!"
+			puts e
 			return 0
 		end
 	end
